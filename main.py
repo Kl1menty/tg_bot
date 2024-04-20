a424daabd8bc5d43a9f11ce0bf00c02be3c62271
Kl1menty <kliment2204@gmail.com>
2024-04-20 22:40:51 +0500

    Добавили возможность получать информацию о пользователе

--- main.py
+++ main.py
@@ -9,6 +9,46 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+def date_to_zodiac(date):
+    day, month, year = map(int, date.split('.'))
+
+    if (month == 1 and day >= 21) or (month == 2 and day <= 20):
+        return "Водолей"
+
+    elif (month == 2 and day >= 21) or (month == 3 and day <= 20):
+        return "Рыбы"
+
+    elif (month == 3 and day >= 21) or (month == 4 and day <= 20):
+        return "Овен"
+
+    elif (month == 4 and day >= 21) or (month == 5 and day <= 20):
+        return "Телец"
+
+    elif (month == 5 and day >= 21) or (month == 6 and day <= 21):
+        return "Близнецы"
+
+    elif (month == 6 and day >= 22) or (month == 7 and day <= 22):
+        return "Рак"
+
+    elif (month == 7 and day >= 23) or (month == 8 and day <= 23):
+        return "Лев"
+
+    elif (month == 8 and day >= 24) or (month == 9 and day <= 23):
+        return "Дева"
+
+    elif (month == 9 and day >= 24) or (month == 10 and day <= 23):
+        return "Весы"
+
+    elif (month == 10 and day >= 24) or (month == 11 and day <= 22):
+        return "Скорпион"
+
+    elif (month == 11 and day >= 23) or (month == 12 and day <= 21):
+        return "Стрелец"
+
+    elif (month == 12 and day >= 22) or (month == 1 and day <= 20):
+        return "Козерог"
+
+
 async def start(update, context):
     reply_keyboard = [['/data', '/zodiac', '/natal'], ['/horoscope', '/compatibility']]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
@@ -47,9 +87,95 @@ async def text_handler(update, context):
     await update.message.reply_text("Введите соответствующую команду.")
 
 
+async def questionary(update, context):
+    await update.message.reply_text("Опрос начался, для досрочного завершения вызовите /stop")
+    await update.message.reply_text("Какой у вас пол?(м/ж)")
+    return 1
+
+
+gender = ''
+
+
+async def first_response(update, context):
+    global gender
+    mes = update.message.text.lower()
+
+    if mes in ['м', 'ж']:
+        gender = mes
+        await update.message.reply_text("Введите свою дату рождения\n"
+                                        "Пример: 23.04.2024")
+        return 2
+
+    else:
+        await update.message.reply_text("Внимательно посмотрите пример и попробуйте еще раз")
+        return 1
+
+
+async def second_response(update, context):
+    user = update.effective_user
+    mes = update.message.text
+
+    try:
+        day, month, year = [int(i) for i in mes.split('.')]
+
+        if day in range(1, 32) and month in range(1, 13) and year in range(1900, 2025):
+            with open('user_data.csv', 'r', encoding="utf8") as csvfile:
+                reader = list(csv.DictReader(csvfile, delimiter=';', quotechar='"'))
+
+            if str(user.id) in [i['id'] for i in reader]:
+                for i in range(len(reader)):
+                    if reader[i]['id'] == str(user.id):
+                        reader[i] = {
+                            'id': user.id,
+                            'user_name': user.username,
+                            'gender': gender,
+                            'date': mes,
+                            'zodiac': date_to_zodiac(mes)
+                        }
+                with open('user_data.csv', 'w', newline='', encoding="utf8") as csvfile:
+                    writer = csv.DictWriter(csvfile,
+                                            fieldnames=list(reader[0].keys()),
+                                            delimiter=';',
+                                            quoting=csv.QUOTE_NONNUMERIC)
+                    writer.writeheader()
+                    for row in reader:
+                        writer.writerow(row)
+            else:
+                with open('user_data.csv', 'a', newline='', encoding="utf8") as csvfile:
+                    writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    writer.writerow([user.id, user.username, gender, mes, date_to_zodiac(mes)])
+
+            await update.message.reply_text("Спасибо! Для продолжения введите соответствующую команду.")
+            return ConversationHandler.END
+
+        else:
+            await update.message.reply_text("Укажите существующую дату")
+            return 2
+
+    except:
+        await update.message.reply_text("Внимательно посмотрите пример и попробуйте еще раз")
+        return 2
+
+
+async def stop(update, context):
+    await update.message.reply_text("Опрос окончен")
+    return ConversationHandler.END
+
+
 def main():
     token_tgb = '7193208629:AAE4UJ5w3dlRZGVJvPe5PRNVbWzMjEClIwQ'
     application = Application.builder().token(token_tgb).build()
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('questionary', questionary)],
+        states={
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)],
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    application.add_handler(conv_handler)
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
